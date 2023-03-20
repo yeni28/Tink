f30151e55b01e28b4a2e634ee806ea8e4d2ee302
박준영 <bjy9507@naver.com>
2023-03-20 17:48:43 +0900

    [BE] feat: 인메모리 사용자 추가
    - security userDetailManager 메소드 추가
    
    이슈: S08P22C201-603

--- back/tink/src/main/java/com/ssafy/tink/config/SecurityConfig.java
+++ back/tink/src/main/java/com/ssafy/tink/config/SecurityConfig.java
@@ -5,6 +5,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.jaas.memory.InMemoryConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -14,7 +18,11 @@ public class SecurityConfig{
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
 
-		http.csrf().disable();
+		http.csrf().disable()			// csrf 해제
+			.formLogin().disable()		// form으로 로그인하는 방식 해제
+			.httpBasic().disable()		//
+			.sessionManagement()		// session 설정관리
+			.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
 		http.authorizeRequests()
 			.antMatchers("/").permitAll()
@@ -23,6 +31,15 @@ public class SecurityConfig{
 			.anyRequest().authenticated();
 
 		return http.build();
+	}
+
+	public InMemoryUserDetailsManager userDetailsManager() {
+		UserDetails user = User.withDefaultPasswordEncoder()
+			.username("admin")
+			.password("ssafy")
+			.roles("USER")
+			.build();
+		return new InMemoryUserDetailsManager(user);
 	}
 
 }
