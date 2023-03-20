ca5ddc7e6f392d2108b3dd7e4fbf1858b5dc5355
박준영 <bjy9507@naver.com>
2023-03-20 14:28:53 +0900

    [BE] feat: mysql 설정
    - MYSQL connection lib 추가
    - JPA starter lib 추가
    - mysql driver, url, username, password 추가
    - application 설정 파일 분산
    
    이슈: S08P22C201-603

--- back/tink/src/main/java/com/ssafy/tink/TinkApplication.java
+++ back/tink/src/main/java/com/ssafy/tink/TinkApplication.java
@@ -2,12 +2,18 @@ package com.ssafy.tink;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class TinkApplication {
-
+	private static final String properties = "spring.config.location="
+		+ "classpath:application-common.yml,"
+		+ " classpath:application-local.yml,"
+		+ " classpath:application-dev.yml";
 	public static void main(String[] args) {
-		SpringApplication.run(TinkApplication.class, args);
+		new SpringApplicationBuilder(TinkApplication.class)
+				.properties(properties)
+				.run(args);
 	}
 
 }
