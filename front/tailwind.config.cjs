c94a1de65c43ff2e5d4bd1ede11450400854f981
심재원 <csappletail@gmail.com>
2023-03-20 16:13:06 +0900

    temp

--- front/tailwind.config.cjs
+++ front/tailwind.config.cjs
@@ -1,8 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        mint: '#CBECE4',
+        red: '#E78078',
+        pink: '#EBC6CE',
+        beige: '#FFF7EC',
+        white: '#FFFFFF',
+        warn: '#EB2B1D',
+        line: '#DFDBD2',
+        black: '#252525',
+        link: '#7694FE',
+        grey: '#767676',
+      },
+      fontFamily: {
+        sans: ['Pretendard'],
+        pop: ['Poppins'],
+        cha: ['KCC-Chassam'],
+      },
+    },
   },
   plugins: [],
 }
