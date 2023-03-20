f913da2ccc1f6777330ecb1cc27f2889dd76f584
박준영 <bjy9507@naver.com>
2023-03-20 11:29:48 +0900

    [BE] feat: swagger2 설정
    - swagger에 필요한 library 추가
    - add SwaggerConfig.class
    
    이슈: S08P22C201-603

---
+++ back/tink/src/main/java/com/ssafy/tink/TinkApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.tink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TinkApplication.class, args);
+	}
+
+}
