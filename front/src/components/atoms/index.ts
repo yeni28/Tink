1d03367f59d506e81afb653518081a31f7625706
정하림 <harimchung1998@gmail.com>
2023-03-21 09:27:13 +0900

    [FE] refactor : index.ts refactor
    이슈: S08P22C201-

--- front/src/components/atoms/index.ts
+++ front/src/components/atoms/index.ts
@@ -1,9 +1,8 @@
 import ImageSm from './ImageSm'
 
-export const atoms = { ImageSm }
 import ImageLg from '@/components/atoms/ImageLg'
 import ImageMd from '@/components/atoms/ImageMd'
 
-const atoms = { ImageMd, ImageLg }
+const atoms = { ImageSm, ImageMd, ImageLg }
 
 export default atoms
