4b3ca0dff3a2e51880dfffef007bef8e8008e8df
박준영 <bjy9507@naver.com>
2023-03-21 17:51:30 +0900

    [BE] feat: OAuth2Service 추가
    - CustomOAuth2UserService.class 추가
    - OAuth2 Process 예외처리 클래스 생성
    
    이슈: S08P22C201-

--- back/tink/src/main/java/com/ssafy/tink/test/Sample.java
+++ back/tink/src/main/java/com/ssafy/tink/test/Sample.java
@@ -1,13 +1,40 @@
 package com.ssafy.tink.test;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import com.ssafy.tink.config.msg.AuthProvider;
+import com.ssafy.tink.config.msg.MemberRole;
+
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
 
 @Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class Sample {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	@Column
 	private String email;
+	@Column
 	private String name;
+
+	@Enumerated(EnumType.STRING)
+	private MemberRole	role;
+	@Enumerated(EnumType.STRING)
+	private AuthProvider authProvider;
 }
