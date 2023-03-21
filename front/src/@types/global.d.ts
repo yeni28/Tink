c825154168926a33e729ac9b437a2996718e0f33
정하림 <harimchung1998@gmail.com>
2023-03-21 10:05:48 +0900

    [FE] feat: 하트 버튼 추]
    - onClick function 추
    
    이슈: S08P22C201-657

--- front/src/@types/global.d.ts
+++ front/src/@types/global.d.ts
@@ -2,10 +2,3 @@ declare module '*.css' {
   const styleObject: { [key: string]: string }
   export default styleObject
 }
-
-interface imgProps {
-  src: string
-  alt?: string
-  width?: number
-  height?: number
-}
