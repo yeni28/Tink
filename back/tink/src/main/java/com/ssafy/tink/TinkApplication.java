e2b1520745601d9c39bedf8fbd6c00f8c72d125e
박준영 <bjy9507@naver.com>
2023-03-21 15:02:07 +0900

    [BE] feat: .yml 설정 추가
    - 환경에 맞는 .yml 설정 할 수 있도록 추가
    - spring.profiles 내용을 로컬: local 개발환경: dev
    
    이슈: S08P22C201-603

--- back/tink/src/main/java/com/ssafy/tink/TinkApplication.java
+++ back/tink/src/main/java/com/ssafy/tink/TinkApplication.java
@@ -6,14 +6,7 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class TinkApplication {
-	private static final String properties = "spring.config.location="
-		+ "classpath:application-common.yml,"
-		+ " classpath:application-local.yml,"
-		+ " classpath:application-dev.yml";
 	public static void main(String[] args) {
-		new SpringApplicationBuilder(TinkApplication.class)
-				.properties(properties)
-				.run(args);
+		SpringApplication.run(TinkApplication.class,args);
 	}
-
 }
