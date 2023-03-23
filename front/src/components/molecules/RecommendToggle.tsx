a93ef6838b9b25b037107e609beb784167bf81d9
심재원 <csappletail@gmail.com>
2023-03-23 17:46:31 +0900

    [FE] feat: toggle에 따른 버튼 모양 변경 작업중
    이슈: S08P22C201-636

--- front/src/components/molecules/RecommendToggle.tsx
+++ front/src/components/molecules/RecommendToggle.tsx
@@ -1,22 +1,36 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import { NavLink, useLocation } from 'react-router-dom'
+
+import atoms from '@/components/atoms'
 
 function RecommendToggle() {
+  const [isSelecteds, setIsSelecteds] = useState({
+    isPatternSelected: true,
+    isYarnSelected: false,
+    isColorSelected: false,
+  })
+
   return (
-    <div className="flex justify-center">
-      <div className="text-red cursor-pointer justify-self-start inline-block">
+    <div className="flex justify-center gap-44">
+      <div className="cursor-pointer justify-self-start inline-block">
         <NavLink to="/recommend/select/pattern">
-          <div>패턴 추천</div>
+          {isSelecteds.isPatternSelected ? (
+            <button className="w-[7.31rem] h-[2.94rem]" type="button">
+              패턴 추천
+            </button>
+          ) : (
+            <div className="w-[7.31rem] h-[2.94rem]">패턴 추천</div>
+          )}
         </NavLink>
       </div>
-      <div className="text-red cursor-pointer justify-self-start inline-block">
+      <div className="cursor-pointer justify-self-start inline-block">
         <NavLink to="/recommend/select/yarn">
-          <div>실 추천</div>
+          <div className="w-[7.31rem] h-[2.94rem]">실 추천</div>
         </NavLink>
       </div>
-      <div className="text-red cursor-pointer justify-self-start inline-block">
+      <div className="cursor-pointer justify-self-start inline-block">
         <NavLink to="/recommend/select/color">
-          <div>색 조합 추천</div>
+          <div className="w-[7.31rem] h-[2.94rem]">색 조합 추천</div>
         </NavLink>
       </div>
     </div>
