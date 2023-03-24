8e9a08990e1e22025656130cc2e8c45e4362558b
박준영 <bjy9507@naver.com>
2023-03-24 22:11:15 +0900

    [BE] feat: swagger Authentication 기능
    - 토큰 입력할 수 있는 버튼 추가
    
    이슈: S08P22C201-605

--- back/tink/src/main/java/com/ssafy/tink/config/SwaggerConfig.java
+++ back/tink/src/main/java/com/ssafy/tink/config/SwaggerConfig.java
@@ -1,27 +1,48 @@
 package com.ssafy.tink.config;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
+import lombok.RequiredArgsConstructor;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
+@RequiredArgsConstructor
 public class SwaggerConfig extends WebMvcConfigurationSupport {
+
+	private static final String SERVICE_TITLE = "Tink";
+	private static final String SERVICE_DESCRIPTION = "뜨개질 추천 사이트";
+	private static final String SERVICE_VERSION = "1.0.0";
 	@Bean
 	public Docket api() {
 		return new Docket(DocumentationType.SWAGGER_2)
 			.useDefaultResponseMessages(false)
+			.securityContexts(Arrays.asList(customSecurityContext()))
+			.securitySchemes(Arrays.asList(apiKey()))
 			.select()
-			.apis(RequestHandlerSelectors.any())
+			.apis(RequestHandlerSelectors.
+					basePackage("com.ssafy.tink"))
 				.paths(PathSelectors.any())
-			.build();
+			.build()
+			.apiInfo(apiInfo());
 	}
 
 	@Override
@@ -31,5 +52,32 @@ public class SwaggerConfig extends WebMvcConfigurationSupport {
 		registry.addResourceHandler("/webjars/**").
 			addResourceLocations("classpath:/META-INF/resources/webjars/");
 		super.addResourceHandlers(registry);
+	}
+
+	// Authentication key 활성화 설정
+	private ApiKey apiKey() {
+		return new ApiKey("Authorization", "Authorization", "header");
+	}
+
+	private SecurityContext customSecurityContext() {
+		return SecurityContext.builder()
+			.securityReferences(defaultAuth())
+			.build();
+	}
+
+	private List<SecurityReference> defaultAuth() {
+		AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+		AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+		authorizationScopes[0] = authorizationScope;
+		return Arrays.asList(new SecurityReference("Authorization", authorizationScopes));
+	}
+
+	private ApiInfo apiInfo() {
+		return new ApiInfoBuilder()
+			.title(SERVICE_TITLE)
+			.description(SERVICE_DESCRIPTION)
+			.version(SERVICE_VERSION)
+			.contact(new Contact("welcome to tink", "http://example.com", "example@tink.com"))
+			.build();
 	}
 }
