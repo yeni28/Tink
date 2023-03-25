d1eb9c8d9661744a5002322bb22f5c920c65c7e9
박준영 <bjy9507@naver.com>
2023-03-25 22:03:32 +0900

    [BE] comment: securityUtil 주석 추가
    이슈:

--- back/tink/src/main/java/com/ssafy/tink/config/Util/SecurityUtil.java
+++ back/tink/src/main/java/com/ssafy/tink/config/Util/SecurityUtil.java
@@ -16,20 +16,25 @@ import lombok.extern.slf4j.Slf4j;
 public class SecurityUtil {
 
 	public static Optional<String> getAuthenticationInfo() {
+		// 사용자 정보를 가지고있는 authentication 객체를 가져와야합니다.
 		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-
+		// 만약 정보를 가져오지 못하였으면 빈 Optional 객체를 반환
 		if( authentication == null ) {
 			log.debug("sorry, Authentication not found");
 			return Optional.empty();
 		}
-
+		// 사용자 ID를 저장할 변수지정
 		String memberId = null;
+		// authentication 객체의 타입이 OAuth2User인지 확인
 		if( authentication.getPrincipal() instanceof OAuth2UserDetail) {
+			// authentication 객체에 존재하는 OAuth2UserDetail 객체를 가져온다.
 			OAuth2UserDetail userDetail = (OAuth2UserDetail)authentication.getPrincipal();
+			// memberId를 저장
 			memberId = String.valueOf(userDetail.getId());
-		} else if( authentication.getPrincipal() instanceof String ) {
-			memberId = (String)authentication.getPrincipal();
+			return Optional.of(memberId);
+		} else {
+			log.debug("sorry, what is authentication Type?");
+			return Optional.empty();
 		}
-		return Optional.of(memberId);
 	}
 }
