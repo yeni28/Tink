a1e24f7ee10e2f709ba803d78475204781f05b61
정하림 <harimchung1998@gmail.com>
2023-03-27 17:08:40 +0900

    [FE] feat: 처음 선택 페이지 제작중
    - linebox 이름 변경 => patternLineBox
    - global css에서 img max-width: 200%로 설정. 추후에 변경 가능
    - first favorite page img 위치 조정중
    
    이슈: S08P22C201-729

--- front/src/pages/recommend/first/favorite/index.tsx
+++ front/src/pages/recommend/first/favorite/index.tsx
@@ -1,20 +1,35 @@
 import React from 'react'
 
-import atoms from '@/components/atoms'
+import { MdOutlineRefresh } from 'react-icons/md'
+
+import patternLineBox from '@/assets/drawings/patternLineBox.png'
 import organisms from '@/components/organisms'
 
 function FavoriteFirstRecommend() {
   return (
-    <div>
+    <div className="relative flex justify-center">
+      {/* 패턴 영역 */}
+      <img
+        alt="linebox"
+        className="absolute"
+        src={patternLineBox}
+        width="105%"
+      />
       <div className="bg-white px-4 pt-7">
         <organisms.Patterns />
+        {/* 하단 영역 */}
         <div className="mt-[4.65rem] flex justify-center pb-12 mb-16">
-          <atoms.ButtonTag
-            bgColor="red"
-            innerValue="다른 도안"
-            textColor="white"
+          {/* 버튼 */}
+          <button
+            type="button"
+            className={
+              'flex justify-center gap-1 items-center rounded-[3.13rem] text-title3 px-[0.88rem] py-[0.31rem] bg-red text-white'
+            }
             onClick={() => console.log('refresh')}
-          />
+          >
+            다른 도안
+            <MdOutlineRefresh />
+          </button>
         </div>
       </div>
     </div>
