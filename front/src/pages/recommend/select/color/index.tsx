a3b9f912bcf911378afc1b5896ff69560b07fa3a
심재원 <csappletail@gmail.com>
2023-03-27 14:03:35 +0900

    [FE] feat: 색추천메인 제작중
    - 테두리 그림, 고양이 그림 추가
    - 그림 클릭시 사진 넣기 가능
    사진 넣을시 이미지 프리뷰 있음
    
    이슈: S08P22C201-731

--- front/src/pages/recommend/select/color/index.tsx
+++ front/src/pages/recommend/select/color/index.tsx
@@ -1,7 +1,55 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
+import FileBox from '@/assets/drawings/FileLineBox.png'
+import FileCat from '@/assets/drawings/파일냥이.png'
 
 function ColorSelectRecommend() {
-  return <div>ColorSelectRecommend</div>
+  const [imgPreview, setImgPreview] = useState<any>()
+  const [image, setImage] = useState<any>()
+
+  const onChangeFile = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setImage(e.target.files)
+  }
+
+  useEffect(() => {
+    if (image && image.length > 0) {
+      const file = image[0]
+      console.log(file)
+      setImgPreview(URL.createObjectURL(file))
+    }
+  }, [image])
+
+  return (
+    <div className="flex justify-center items-center pt-16">
+      <div className="relative flex justify-center">
+        <img alt="border" src={FileBox} width="50%" />
+        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+          <input
+            accept="image/*"
+            className="hidden"
+            id="picture"
+            type="file"
+            onChange={onChangeFile}
+          />
+          <label htmlFor="picture">
+            {(imgPreview && (
+              <img
+                alt="preview"
+                className="object-cover object-cover w-full cursor-pointer"
+                src={imgPreview}
+              />
+            )) || (
+              <img
+                alt="preview"
+                className="object-cover cursor-pointer w-[196px] h-[209px]"
+                src={FileCat}
+              />
+            )}
+          </label>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default ColorSelectRecommend
