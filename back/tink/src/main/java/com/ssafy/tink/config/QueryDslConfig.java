3e72f179cea72bf265b3781da726ea6753c32be2
이해민 <2haemin98@gmail.com>
2023-03-29 15:04:43 +0900

    [BE] application-test 파일 생성
    test에 사용될 application 파일 생성
    
    이슈: S08P22C201-705

--- back/tink/src/main/java/com/ssafy/tink/config/QueryDslConfig.java
+++ back/tink/src/main/java/com/ssafy/tink/config/QueryDslConfig.java
@@ -1,6 +1,6 @@
 package com.ssafy.tink.config;
 
-<<<<<<< HEAD
+
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,25 +14,6 @@ import javax.persistence.PersistenceContext;
 @Configuration
 public class QueryDslConfig {
 
-    @PersistenceContext
-    private EntityManager entityManager;
-
-    @Bean
-    public JPAQueryFactory jpaQueryFactory(){
-        return new JPAQueryFactory(entityManager);
-    }
-=======
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-import com.querydsl.jpa.impl.JPAQueryFactory;
-
-@Configuration
-public class QueryDslConfig {
-
 	@PersistenceContext
 	private EntityManager entityManager;
 
@@ -41,5 +22,4 @@ public class QueryDslConfig {
 		return new JPAQueryFactory(entityManager);
 	}
 
->>>>>>> 4b7288ee17d6cf7af104e64cec1883db19d1b96f
 }
