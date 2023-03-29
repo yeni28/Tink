bfe6fbd0158e861fa35ececfcb2f07b77c1e5ea5
정하림 <harimchung1998@gmail.com>
2023-03-29 16:15:59 +0900

    [FE] feat: gsap 추가 및 knit page 연습 중
    이슈: S08P22C201-

--- front/src/pages/tutorial/knit/index.tsx
+++ front/src/pages/tutorial/knit/index.tsx
@@ -1,7 +1,67 @@
-import React from 'react'
+import React, { useEffect, useRef, useState, useCallback } from 'react'
+
+import { gsap } from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
 
 function KnitTutorial() {
-  return <div>KnitTutorial</div>
+  const divRef = useRef<any>(null)
+  const [height, setHeight] = useState(0)
+
+  useEffect(() => {
+    if (divRef.current) {
+      setHeight(divRef.current.offsetHeight)
+    }
+
+    gsap.registerPlugin(ScrollTrigger)
+    gsap.utils.toArray<any>('section').forEach((section, i) => {
+      ScrollTrigger.create({
+        trigger: section,
+        start: 'top 250',
+        end: 'bottom bottom',
+        markers: true,
+        pin: true,
+        pinSpacing: false,
+      })
+    })
+    ScrollTrigger.create({
+      snap: 'labels',
+    })
+  }, [])
+
+  return (
+    <div className="h-screen">
+      <section
+        ref={divRef}
+        className="w-full h-full relative flex justify-center items-center bg-orange-400"
+      >
+        <h1>section01</h1>
+      </section>
+      <section
+        ref={divRef}
+        className="w-full h-full relative flex justify-center items-center bg-green-200"
+      >
+        <h1>section02</h1>
+      </section>
+      <section
+        ref={divRef}
+        className="w-full h-full relative flex justify-center items-center bg-fuchsia-300"
+      >
+        <h1>section03</h1>
+      </section>
+      <section
+        ref={divRef}
+        className="w-full h-full relative flex justify-center items-center bg-blue-200"
+      >
+        <h1>section04</h1>
+      </section>
+      <section
+        ref={divRef}
+        className="w-full h-full relative flex justify-center items-center bg-amber-300"
+      >
+        <h1>section05</h1>
+      </section>
+    </div>
+  )
 }
 
 export default KnitTutorial
--- front/src/routes/RootLayout.tsx
+++ front/src/routes/RootLayout.tsx
@@ -1,10 +1,22 @@
-import { Outlet } from 'react-router-dom'
+import { useEffect } from 'react'
+import { useLocation, Outlet } from 'react-router-dom'
+
+import gsap from 'gsap'
+import ScrollTrigger from 'gsap/ScrollTrigger'
 
 import organisms from '@/components/organisms'
 
 import Detail from '@/routes/detail/Detail'
 
 export default function RootLayout() {
+  const location = useLocation()
+  useEffect(() => {
+    ScrollTrigger.getAll().forEach((instance) => {
+      instance.kill()
+    })
+    // This in case a scroll animation is active while the route is updated
+    gsap.killTweensOf(window)
+  }, [location])
   return (
     <>
       <organisms.Navbar />
