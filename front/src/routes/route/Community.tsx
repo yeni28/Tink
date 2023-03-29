870156b4f7a1727689f5dde06910a07a5fcef150
심재원 <csappletail@gmail.com>
2023-03-29 14:11:48 +0900

    [FE] feat: 헤더 로직 수정
    - 헤더 로직
    헤더데이터에 없는 값은 헤더 컴포넌트 안나오게 수정
    
    이슈: S08P22C201-736

--- front/src/routes/route/Community.tsx
+++ front/src/routes/route/Community.tsx
@@ -1,15 +1,17 @@
 import React from 'react'
 
 import CommunityMainPage from '@/pages/community'
-import GroupCommunity from '@/pages/community/group'
 
+import GroupCommunity from '@/pages/community/group'
 import DetailGroupCommunity from '@/pages/community/group/detail'
 import ListGroupCommunity from '@/pages/community/group/list'
 import WriteGroupCommunity from '@/pages/community/group/write'
+
 import QuestionCommunity from '@/pages/community/question'
 import DetailQuestionCommunity from '@/pages/community/question/detail'
 import ListQuestionCommunity from '@/pages/community/question/list'
 import WriteQuestionCommunity from '@/pages/community/question/write'
+
 import ReviewCommunity from '@/pages/community/review'
 import DetailReviewCommunity from '@/pages/community/review/detail'
 import ListReviewCommunity from '@/pages/community/review/list'
@@ -23,29 +25,32 @@ const Community = [
       {
         path: 'group',
         element: <GroupCommunity />,
-        children: [{ path: 'list', element: <ListGroupCommunity /> }],
+        children: [
+          { path: 'list', element: <ListGroupCommunity /> },
+          { path: 'detail/:id', element: <DetailGroupCommunity /> },
+          { path: 'write', element: <WriteGroupCommunity /> },
+        ],
       },
       {
         path: 'question',
         element: <QuestionCommunity />,
-        children: [{ path: 'list', element: <ListQuestionCommunity /> }],
+        children: [
+          { path: 'list', element: <ListQuestionCommunity /> },
+          { path: 'detail/:id', element: <DetailQuestionCommunity /> },
+          { path: 'write', element: <WriteQuestionCommunity /> },
+        ],
       },
       {
         path: 'review',
         element: <ReviewCommunity />,
-        children: [{ path: 'list', element: <ListReviewCommunity /> }],
+        children: [
+          { path: 'list', element: <ListReviewCommunity /> },
+          { path: 'write', element: <WriteReviewCommunity /> },
+        ],
       },
     ],
   },
-  { path: 'community/question/write', element: <WriteQuestionCommunity /> },
-  {
-    path: 'community/question/detail/:id',
-    element: <DetailQuestionCommunity />,
-  },
-  { path: 'community/group/write', element: <WriteGroupCommunity /> },
-  { path: 'community/group/detail/:id', element: <DetailGroupCommunity /> },
-  { path: 'community/review/detail/:id', element: <DetailReviewCommunity /> },
-  { path: 'community/review/write', element: <WriteReviewCommunity /> },
+  { path: '/community/review/detail/:id', element: <DetailReviewCommunity /> },
 ]
 
 export default Community
