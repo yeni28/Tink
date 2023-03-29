5af22eb787aaaeadbca175854014ea81583f9acf
정하림 <harimchung1998@gmail.com>
2023-03-29 17:55:05 +0900

    [FE] feat: scroll animation 연구중, sprite image 추가
    이슈: S08P22C201-

--- front/src/pages/tutorial/knit/index.tsx
+++ front/src/pages/tutorial/knit/index.tsx
@@ -1,64 +1,24 @@
-import React, { useEffect, useRef, useState, useCallback } from 'react'
+import React, { useEffect, useRef } from 'react'
 
-import { gsap } from 'gsap'
+import gsap from 'gsap'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
 
+import purl from '@/assets/sprite/purl_sprite.png'
+
+gsap.registerPlugin(ScrollTrigger)
 function KnitTutorial() {
-  const divRef = useRef<any>(null)
-  const [height, setHeight] = useState(0)
-
-  useEffect(() => {
-    if (divRef.current) {
-      setHeight(divRef.current.offsetHeight)
-    }
-
-    gsap.registerPlugin(ScrollTrigger)
-    gsap.utils.toArray<any>('section').forEach((section, i) => {
-      ScrollTrigger.create({
-        trigger: section,
-        start: 'top 250',
-        end: 'bottom bottom',
-        markers: true,
-        pin: true,
-        pinSpacing: false,
-      })
-    })
-    ScrollTrigger.create({
-      snap: 'labels',
-    })
-  }, [])
-
+  const imgRef = useRef(null)
   return (
-    <div className="h-screen">
-      <section
-        ref={divRef}
-        className="w-full h-full relative flex justify-center items-center bg-orange-400"
-      >
-        <h1>section01</h1>
-      </section>
-      <section
-        ref={divRef}
-        className="w-full h-full relative flex justify-center items-center bg-green-200"
-      >
-        <h1>section02</h1>
-      </section>
-      <section
-        ref={divRef}
-        className="w-full h-full relative flex justify-center items-center bg-fuchsia-300"
-      >
-        <h1>section03</h1>
-      </section>
-      <section
-        ref={divRef}
-        className="w-full h-full relative flex justify-center items-center bg-blue-200"
-      >
-        <h1>section04</h1>
-      </section>
-      <section
-        ref={divRef}
-        className="w-full h-full relative flex justify-center items-center bg-amber-300"
-      >
-        <h1>section05</h1>
+    <div>
+      {/* 이미지 영역 */}
+      <section className="h-screen bg-pink">
+        <div className="h-full mx-auto w-full bg-no-repeat bg-center bg-[url('@/assets/sprite/purl_sprite.png')]"></div>
+        <img
+          ref={imgRef}
+          alt="purl_sprite"
+          className="purlSprite"
+          src={purl}
+        ></img>
       </section>
     </div>
   )
