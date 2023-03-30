3cd67a2fadbdb2a2cb6b54a33e24406c159ce233
김예은 <suntofu0228@naver.com>
2023-03-30 13:48:07 +0900

    [FE] design: 자랑글 디테일 css 수정
    이슈: S08P22C201-

--- front/src/pages/community/review/detail/index.tsx
+++ front/src/pages/community/review/detail/index.tsx
@@ -4,6 +4,9 @@ import { useParams } from 'react-router-dom'
 import { switchFollow } from './apis/follow'
 import review_detail_dummy from './dummydata'
 
+import reviewDetailBox from '@/assets/drawings/reviewDetailBox.png'
+import shortline from '@/assets/drawings/shortline.png'
+
 import atoms from '@/components/atoms'
 
 function DetailReviewCommunity() {
@@ -42,7 +45,88 @@ function DetailReviewCommunity() {
           onClick={() => switchFollow(follow, setFollow)}
         />
       </div>
-      <div id="information"></div>
+      <div className="flex flex-col items-center">
+        <div>
+          <img alt="detailbox" className="relative" src={reviewDetailBox} />
+        </div>
+        <div className=" justify-center absolute mt-[8rem]">
+          <div className="flex" id="information">
+            <div className="grid grid-cols-2 gap-10">
+              {/* 실 이름 */}
+              <div className="flex">
+                <div className="mr-[1rem]" id="info-name">
+                  <p className="text-headline-bold">실 이름</p>
+                </div>
+                <div id="info-content">
+                  <div className="content-center">
+                    <p className="w-[12rem] text-center text-body">
+                      {review?.yarnName}
+                    </p>
+                    <img alt="하단 선" className="w-[12rem]" src={shortline} />
+                  </div>
+                </div>
+              </div>
+              {/* 사용한 바늘 */}
+              <div className="flex">
+                <div className="mr-[1rem]" id="info-name">
+                  <p className="text-headline-bold">사용한 바늘</p>
+                </div>
+                <div id="info-content">
+                  <div className="content-center">
+                    <p className="w-[12rem] text-center text-body">
+                      {review?.needle}
+                    </p>
+                    <img alt="하단 선" className="w-[12rem]" src={shortline} />
+                  </div>
+                </div>
+              </div>
+
+              {/* 실 길이/무게 */}
+              <div className="flex">
+                <div className="mr-[1rem]" id="info-name">
+                  <p className="text-headline-bold">실 길이/무게</p>
+                </div>
+                <div id="info-content">
+                  <div className="content-center">
+                    <p className="w-[12rem] text-center text-body">
+                      {review?.yarnLength} m / {review?.yarnWeight} g
+                    </p>
+                    <img alt="하단 선" className="w-[12rem]" src={shortline} />
+                  </div>
+                </div>
+              </div>
+              {/* 소요기간 */}
+              <div className="flex">
+                <div className="mr-[1rem]" id="info-name">
+                  <p className="text-headline-bold">소요기간</p>
+                </div>
+                <div id="info-content">
+                  <div className="content-center">
+                    <p className="w-[12rem] text-center text-body">
+                      {review?.time}
+                    </p>
+                    <img alt="하단 선" className="w-[12rem]" src={shortline} />
+                  </div>
+                </div>
+              </div>
+            </div>
+            {/* 도안 */}
+            <div className="flex ml-12">
+              <div className="mr-[1rem]" id="info-name">
+                <p className="text-headline-bold">도안</p>
+              </div>
+              <div id="info-content">
+                <div
+                  className="w-[9rem] h-[9rem] rounded-[1rem] bg-cover bg-center"
+                  style={{
+                    backgroundImage: `url(${review?.pattern.patternImage})`,
+                  }}
+                ></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
