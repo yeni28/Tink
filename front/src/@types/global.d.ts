3d45ef316dd4764336f903ff67649127f6be5a9a
심재원 <csappletail@gmail.com>
2023-03-30 15:37:48 +0900

    [FE] feat: interface 완성
    - 패턴부분 제외 완성
    
    이슈: 없음

--- front/src/@types/global.d.ts
+++ front/src/@types/global.d.ts
@@ -2,3 +2,18 @@ declare module '*.css' {
   const styleObject: { [key: string]: string }
   export default styleObject
 }
+
+type CommunityFilter = '최신순' | '인기순'
+type PatternFilter = '최신순' | '인기순' | '이름순'
+
+interface Alarm {
+  commentCnt: number // 알림 총개수
+  comments: {
+    notificationId: number
+    boardCategory: boardCategory
+    title: string // 질문, 커뮤니티 글의 제목
+    commentId: number
+    createdDate: string
+    readStatus: 0 | 1 // (0:안읽음,   1: 읽음)
+  }[]
+}
