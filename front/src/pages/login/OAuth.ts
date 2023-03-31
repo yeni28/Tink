c8c3c304814733e2ce7d7cda83d68da7cd999471
김예은 <suntofu0228@naver.com>
2023-03-31 22:07:06 +0900

    [FE] feat: 소모임/질문 리스트/디테일 추가
    - 소모임 및 질문글 리스트 더미데이터 추가 및 조회 가능
    - 소모임 및 질문글 디테일 더미데이터 추가 및 조회 가능
    
    이슈: S08P22C201-750

---
+++ front/src/pages/login/OAuth.ts
@@ -0,0 +1,10 @@
+// // 테스트 환경
+// export const REDIRECT_URI = 'http://localhost:3000/login'
+// export const HOST = `http://localhost:8080`
+
+// 서버
+export const REDIRECT_URI = 'http://j8c201.p.ssafy.io//login'
+export const HOST = `http://j8c201.p.ssafy.io/`
+
+export const CLIENT_ID = 'cb9783828da32ab9af5c189492140e97'
+export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`
--- front/src/pages/login/index.tsx
+++ front/src/pages/login/index.tsx
@@ -1,6 +1,53 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+
+import { useNavigate } from 'react-router-dom'
+
+import axios from 'axios'
+
+import { useRecoilState } from 'recoil'
+
+import { HOST, REDIRECT_URI } from './OAuth'
+
+import { LoginState } from '@/components/atoms/state/LoginState'
 
 function Login() {
+  const navigate = useNavigate()
+
+  const [isLogin, setIsLogin] = useRecoilState<boolean>(LoginState)
+  const token = window.location.href.split('?token=')[1]
+
+  useEffect(() => {
+    if (token) localStorage.setItem('tinktoken', token)
+    if (localStorage.getItem('tinktoken')) setIsLogin(true)
+  }, [])
+
+  useEffect(() => {
+    ;(async () => {
+      // const PARAMS = new URL(document.location).searchParams
+      // const KAKAO_CODE = PARAMS.get('code')
+
+      const backHost = `${HOST}/oauth2/authorization/google?redirect_url=${REDIRECT_URI}`
+      await axios({
+        url: backHost,
+        method: 'GET',
+      })
+        .then((res) => {
+          console.log(res)
+          const token = res.headers.authorization
+          window.localStorage.setItem('token', token)
+          // 로컬스토리지에 토큰이 있으면 메인페이지로 이동
+          // 없으면 로그인 페이지로 리다이렉트
+          if (token) {
+            navigate('/main')
+          } else {
+            navigate('/')
+          }
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+    })()
+  }, [])
   return <div>Login</div>
 }
 
