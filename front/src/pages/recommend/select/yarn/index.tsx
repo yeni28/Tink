a04fd1ccadffa30d14ac2acbe79c342f9bbdc3bc
정하림 <harimchung1998@gmail.com>
2023-03-31 15:24:04 +0900

    [FE] feat: 색 추천 페이지 작성
    이슈: S08P22C201-722

--- front/src/pages/recommend/select/yarn/index.tsx
+++ front/src/pages/recommend/select/yarn/index.tsx
@@ -1,7 +1,25 @@
 import React from 'react'
 
+import InputForm from './components/InputForm'
+
+import yarnLineBox from '@/assets/drawings/yarnLineBox.png'
+
 function YarnSelectRecommend() {
-  return <div>YarnSelectRecommend</div>
+  return (
+    <div className="relative">
+      {/* form */}
+
+      {/* line box */}
+      <div className="absolute top-12 left-36 ">
+        <img src={yarnLineBox} />
+
+        {/* input form */}
+        <div className="absolute top-16 left-40 text-center ">
+          <InputForm />
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default YarnSelectRecommend
