67bb7e0383738fc53820e62c3d414604053787e9
이해민 <2haemin98@gmail.com>
2023-04-03 13:57:58 +0900

    [BE] fix: patternLike 삭제

--- back/tink/src/main/java/com/ssafy/tink/db/repository/PatternLikesRepository.java
+++ back/tink/src/main/java/com/ssafy/tink/db/repository/PatternLikesRepository.java
@@ -1,8 +1,11 @@
 package com.ssafy.tink.db.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.ssafy.tink.db.entity.PatternLikeId;
@@ -10,4 +13,9 @@ import com.ssafy.tink.db.entity.PatternLikes;
 
 @Repository
 public interface PatternLikesRepository extends JpaRepository<PatternLikes, PatternLikeId> {
+
+	@Query(value = "select * from pattern_likes p where p.pattern_id=:patternId and p.member_id=:memberId", nativeQuery = true)
+	Optional<PatternLikes> searchPatternLike(@Param("patternId") int patterId, @Param("memberId") Long memberId);
+
+
 }
