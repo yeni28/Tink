7339d598dc6e660bf60db6e2babc36d9f8a155e6
임지원 <57473228+wonwu@users.noreply.github.com>
2023-04-03 17:51:49 +0900

    [BE] !HOTFIX: 커뮤니티 수정
    이슈: S08P22C201-

--- back/tink/src/main/java/com/ssafy/tink/db/entity/Material.java
+++ back/tink/src/main/java/com/ssafy/tink/db/entity/Material.java
@@ -3,8 +3,10 @@ package com.ssafy.tink.db.entity;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -43,7 +45,7 @@ public class Material {
 
 	private String time;
 
-	@OneToOne
+	@OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
 	@JoinColumn(name = "board_id", referencedColumnName = "board_id", nullable = false)
 	private Board board;
 
