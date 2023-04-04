673605154df21f30a775c5633d2101aa059c4193
임지원 <57473228+wonwu@users.noreply.github.com>
2023-04-04 15:00:20 +0900

    [BE] fix: img multipart로 수정
    이슈: S08P22C201-

--- back/tink/src/main/java/com/ssafy/tink/controller/ImageController.java
+++ back/tink/src/main/java/com/ssafy/tink/controller/ImageController.java
@@ -54,18 +54,20 @@ public class ImageController {
 
 	// 이미지 업로드
 	@PostMapping("/uploadImage")
-	public BaseResponse<Object> uploadImage(MultipartFile multipartFiles[]) {
+	public ResponseEntity<Object> uploadImage(MultipartFile multipartFiles) {
 		try {
-			MultipartFile file = multipartFiles[0];
+			MultipartFile file = multipartFiles;
 
-			String fileId = (new Date().getTime()) + "" + (new Random().ints(1000, 9999).findAny().getAsInt()); // 현재 날짜와 랜덤 정수값으로 새로운 파일명 만들기
+			String fileId = (new Date().getTime()) + "" + (new Random().ints(1000, 9999)
+				.findAny()
+				.getAsInt()); // 현재 날짜와 랜덤 정수값으로 새로운 파일명 만들기
 			String originName = file.getOriginalFilename(); // ex) 파일.jpg
 			String fileExtension = originName.substring(originName.lastIndexOf(".") + 1); // ex) jpg
 			originName = originName.substring(0, originName.lastIndexOf(".")); // ex) 파일
 			long fileSize = file.getSize(); // 파일 사이즈
 
 			File fileSave = new File(UPLOAD_PATH, fileId + "." + fileExtension); // ex) fileId.jpg
-			if(!fileSave.exists()) { // 폴더가 없을 경우 폴더 만들기
+			if (!fileSave.exists()) { // 폴더가 없을 경우 폴더 만들기
 				fileSave.mkdirs();
 			}
 
@@ -76,21 +78,21 @@ public class ImageController {
 			// System.out.println("fileExtension= " + fileExtension);
 			// System.out.println("fileSize= " + fileSize);
 
-			// return new ResponseEntity<Object>("http://localhost:8081/getImage/" + fileId + "/" + fileExtension, HttpStatus.OK);
-			String result = "http://localhost:8081/img/" + fileId + "/" + fileExtension;
-			return BaseResponse.builder()
-				.result(result)
-				.resultCode(HttpStatus.OK.value())
-				.resultMsg("정상적으로 이미지가 업로드되었습니다.")
-				.build();
-
+			return new ResponseEntity<Object>("http://localhost:8081/getImage/" + fileId + "/" + fileExtension,
+				HttpStatus.OK);
+			// String result = "http://localhost:8081/img/" + fileId + "/" + fileExtension;
+			// return BaseResponse.builder()
+			// 	.result(result)
+			// 	.resultCode(HttpStatus.OK.value())
+			// 	.resultMsg("정상적으로 이미지가 업로드되었습니다.")
+			// 	.build();
 		} catch(IOException e) {
-			// return new ResponseEntity<Object>(null, HttpStatus.CONFLICT);
-			return BaseResponse.builder()
-				.result(null)
-				.resultCode(HttpStatus.CONFLICT.value())
-				.resultMsg("정상적으로 이미지가 업로드되었습니다.")
-				.build();
+			return new ResponseEntity<Object>(null, HttpStatus.CONFLICT);
+			// return BaseResponse.builder()
+			// 	.result(null)
+			// 	.resultCode(HttpStatus.CONFLICT.value())
+			// 	.resultMsg("정상적으로 이미지가 업로드되었습니다.")
+			// 	.build();
 		}
 	}
 }
