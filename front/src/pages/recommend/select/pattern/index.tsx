4e25ceaac938027c35fc0860a6443738319a5041
정하림 <harimchung1998@gmail.com>
2023-04-04 11:32:27 +0900

    [FE] design: toggle button 완성
    이슈: S08P22C201-

--- front/src/pages/recommend/select/pattern/index.tsx
+++ front/src/pages/recommend/select/pattern/index.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
 
+import Attribute from './organisms/Attribute'
 import Category from './organisms/Category'
+import Level from './organisms/Level'
 
 import atoms from '@/components/atoms'
 
@@ -13,10 +15,32 @@ function PatternSelectRecommend() {
     level: false,
   })
   const toggles = [
-    { key: 'pattern', isSelected: option.category, name: '카테고리' },
-    { key: 'yarn', isSelected: option.attribute, name: '속성' },
-    { key: 'color', isSelected: option.level, name: '난이도' },
+    { key: 'category', isSelected: option.category, name: '카테고리' },
+    { key: 'attribute', isSelected: option.attribute, name: '속성' },
+    { key: 'level', isSelected: option.level, name: '난이도' },
   ]
+
+  const changeOption = (name: string) => {
+    if (name === 'category') {
+      setOption({
+        category: true,
+        attribute: false,
+        level: false,
+      })
+    } else if (name === 'attribute') {
+      setOption({
+        category: false,
+        attribute: true,
+        level: false,
+      })
+    } else {
+      setOption({
+        category: false,
+        attribute: false,
+        level: true,
+      })
+    }
+  }
 
   return (
     <div>
@@ -35,6 +59,7 @@ function PatternSelectRecommend() {
                       ? 'bg-grey text-white'
                       : 'border-grey border text-grey'
                   }`}
+                  onClick={() => changeOption(option.key)}
                 >
                   {option.name}
                 </button>
@@ -43,7 +68,13 @@ function PatternSelectRecommend() {
           </div>
 
           {/* 선택영역 */}
-          <Category />
+          {option.attribute ? (
+            <Attribute />
+          ) : option.category ? (
+            <Category />
+          ) : (
+            <Level />
+          )}
 
           {/* 선택 완료 영역 */}
           <div className="absolute -top-20 -right-36 p-4">
