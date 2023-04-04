2e8b41f684d815a107da5d83979bf96c08f5a026
심재원 <csappletail@gmail.com>
2023-04-04 11:15:10 +0900

    [FE] feat: imagehandler 작업중
    - cors 오류 해결 후 진행 예정
    
    이슈: S08P22C201-

--- front/src/pages/login/OAuth.ts
+++ front/src/pages/login/OAuth.ts
@@ -1,7 +1,7 @@
 // // 테스트 환경
-// export const REDIRECT_URI = 'http://localhost:3000/login'
-// export const HOST = `http://localhost:8080`
+export const REDIRECT_URI = 'http://localhost:3000/login'
+export const HOST = `http://localhost:8081`
 
 // 서버
-export const REDIRECT_URI = 'http://j8c201.p.ssafy.io/login'
-export const HOST = `http://j8c201.p.ssafy.io/`
+// export const REDIRECT_URI = 'http://j8c201.p.ssafy.io/login'
+// export const HOST = `http://j8c201.p.ssafy.io/`
