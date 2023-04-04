1f7ed573a3b0efbbf0fcb3890cf3f24e47d11339
정하림 <harimchung1998@gmail.com>
2023-04-04 11:16:40 +0900

    [FE] design : 패턴추천 레이아웃 완성
    이슈: S08P22C201-

--- front/src/pages/recommend/select/pattern/index.tsx
+++ front/src/pages/recommend/select/pattern/index.tsx
@@ -1,7 +1,66 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+import Category from './organisms/Category'
+
+import atoms from '@/components/atoms'
+
+import lace from '@/pages/recommend/select/pattern/components/atoms/lace_down.png'
 
 function PatternSelectRecommend() {
-  return <div>PatternSelectRecommend</div>
+  const [option, setOption] = useState({
+    category: true,
+    attribute: false,
+    level: false,
+  })
+  const toggles = [
+    { key: 'pattern', isSelected: option.category, name: '카테고리' },
+    { key: 'yarn', isSelected: option.attribute, name: '속성' },
+    { key: 'color', isSelected: option.level, name: '난이도' },
+  ]
+
+  return (
+    <div>
+      <div className="mt-[6.5rem] ">
+        <div className="flex relative ">
+          {/* 토글영역 */}
+          <div className="flex flex-col gap-3 pl-9">
+            {toggles.map((option) => {
+              return (
+                <button
+                  key={option.key}
+                  type="button"
+                  className={`w-[5.7rem] h-[2.38rem] rounded-[3.13rem] text-pop text-headline 
+                  ${
+                    option.isSelected
+                      ? 'bg-grey text-white'
+                      : 'border-grey border text-grey'
+                  }`}
+                >
+                  {option.name}
+                </button>
+              )
+            })}
+          </div>
+
+          {/* 선택영역 */}
+          <Category />
+
+          {/* 선택 완료 영역 */}
+          <div className="absolute -top-20 -right-36 p-4">
+            <p className="text-title2-bold">선택 키워드</p>
+            <img src={lace} />
+            <div className="mt-2">
+              <atoms.ButtonTag
+                bgColor="red"
+                innerValue="상의"
+                textColor="white"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default PatternSelectRecommend
---
+++ front/src/pages/recommend/select/pattern/organisms/Attribute.tsx
@@ -0,0 +1,44 @@
+import React from 'react'
+
+import atoms from '@/components/atoms'
+
+import lineBox1 from '@/pages/recommend/select/pattern/components/atoms/line box1.png'
+import lineBox2 from '@/pages/recommend/select/pattern/components/atoms/line box2.png'
+import lineBox3 from '@/pages/recommend/select/pattern/components/atoms/line box3.png'
+
+function Attribute() {
+  const onClickHandler = () => {
+    console.log('Hello')
+  }
+  return (
+    <div>
+      <div className="flex pl-8 relative">
+        {/* 첫번째 카테고리 */}
+        <div className="w-[13.58rem] h-[20.87rem] relative">
+          <img className="w-full h-full" src={lineBox1} />
+          <div className="absolute top-0 left-0"> Attribute Category1</div>
+        </div>
+
+        {/* 두번째 카테고리 */}
+
+        <div className="w-[13.58rem] h-[20.87rem] relative">
+          <img className="w-full h-full" src={lineBox2} />
+          <div className="absolute top-0 left-0">Attribute Category2</div>
+        </div>
+
+        {/* 세번째 카테고리 */}
+        <div className="w-[28.46rem] h-[20.88 rem] relative">
+          <img className="w-full h-full" src={lineBox3} />
+          <div className="absolute top-0 left-0"> Attribute Category3</div>
+        </div>
+
+        {/* 다음 버튼 */}
+        <div className="absolute -bottom-20 -right-14">
+          <atoms.ButtonDoodle innerValue={'다음'} onClick={onClickHandler} />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Attribute
