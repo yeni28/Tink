72e26b3c603fa30911c148d416d5bbe3aad81d12
이해민 <2haemin98@gmail.com>
2023-04-05 14:29:50 +0900

    [CICD] fix: --net 옵션 추가
    --net 옵션 추가
    
    이슈: S08P22C201-

--- back/tink/src/main/java/com/ssafy/tink/TinkApplication.java
+++ back/tink/src/main/java/com/ssafy/tink/TinkApplication.java
@@ -1,15 +1,11 @@
 package com.ssafy.tink;
 
-import javax.annotation.PostConstruct;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
-import com.ssafy.tink.service.CrawlingService;
 
 @EnableFeignClients
 @SpringBootApplication
@@ -17,16 +13,8 @@ import com.ssafy.tink.service.CrawlingService;
 @EnableRedisHttpSession
 public class TinkApplication {
 
-	// @Autowired
-	// private CrawlingService crawlingService;
-
 	public static void main(String[] args) {
 		SpringApplication.run(TinkApplication.class, args);
 	}
-
-	// @PostConstruct
-	// public void runCrawling(){
-	// 	crawlingService.run();
-	// }
 
 }
